77890af1cefbba7f9aaf262c67743c90ed08e2e1
Bryan <bryan@Bryans-MacBook-Pro.local>
2017-05-12 01:59:43 -0700

    text field

--- Swift App Prac 1/ViewController.swift
+++ Swift App Prac 1/ViewController.swift
@@ -13,32 +13,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    
+    @IBOutlet weak var text2: UITextField!
     
     
     @IBAction func theButton(_ sender: Any) {
-     tapCount += 1
-        
-        if tapCount >= 10 {
-            theLabel.text = "Stop Tappin Bitch"
+        print(text1.text!)
+        print(text2.text!)
         }
         
         
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-       
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
